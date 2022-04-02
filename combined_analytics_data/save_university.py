f5952041d3fc243070a121dcbb2893fed2611645
Аналитик psal.ru <87013484+analyst-psalru@users.noreply.github.com>
2022-04-02 18:42:41 +0300

    Данные сайт ученые-исследователи.рф (#3)
    * data: университеты и их страницы на сайте ученые-исследователи.рф
    * fix: у некоторых университетов совпадают аббревиатуры
    * data: вакансии университетов опубликованные на сайте ученые-исследователи.рф
    * feat: отчёт по парсингу вакансий ученые-исследователи.рф
    * docs: оформил ссылки на результаты работы с ученые-исследователи.рф

--- combined_analytics_data/save_university.py
+++ combined_analytics_data/save_university.py
@@ -7,6 +7,7 @@ resp = requests.get('https://lk.priority2030.ru/api/v0/priority/list')
 if resp.status_code == 200:
     json_university = resp.json()
     university = pd.json_normalize(json_university['data']['participants'])
+    university['shortName'] = university.apply(lambda x: '{0} ({1})'.format(x['shortName'], x['city']), axis=1)
 
     # сохраняем в csv-шку
     university.to_csv('data/university.csv', index=False)
