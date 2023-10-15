ef554859d1da6e4efd50b0a127b9456453fab4ce
Alexey Safonov <alexey@safonov.pro>
2023-10-15 17:13:04 +0300

    feat: добавлено описание к чартам

--- PSAL-25_gen_report_on_serverless/report_by_hh_montly.py
+++ PSAL-25_gen_report_on_serverless/report_by_hh_montly.py
@@ -13,11 +13,25 @@ region = df[df['university_abbreviation'] == university][['region', 'id']].group
 #%% Про преподавателей
 
 df_teachers = df[df['professional_roles'] == 'Учитель, преподаватель, педагог'].copy()
+teachers_mean_by_rf = df_teachers['salary'].mean() / 1000
+teachers_mean_by_region = df_teachers[df_teachers['region'] == region]['salary'].mean() / 1000
+teachers_mean_by_university = df_teachers[df_teachers['university_abbreviation'] == university]['salary'].mean() / 1000
+teachers_top_by_university = df_teachers[df_teachers['university_abbreviation'] == university].sort_values(by='salary', ascending=False).iloc[0]
 stat_by_teachers = pd.DataFrame([
-    {'title': 'РФ', 'value': df_teachers['salary'].mean() / 1000},
-    {'title': region, 'value': df_teachers[df_teachers['region'] == region]['salary'].mean() / 1000},
-    {'title': university, 'value': df_teachers[df_teachers['university_abbreviation'] == university]['salary'].mean() / 1000},
+    {'title': 'РФ', 'value': teachers_mean_by_rf},
+    {'title': region, 'value': teachers_mean_by_region},
+    {'title': university, 'value': teachers_mean_by_university},
 ]).dropna()
+data.loc['description_teachers', 'value'] = texts['empty'].format(
+    prof_role='Учитель, преподаватель, педагог'
+) if university not in list(stat_by_teachers['title'].unique()) else texts['not_empty'].format(
+    prof_role='Учитель, преподаватель, педагог',
+    percent_of_rf='{0:0.2%}'.format(teachers_mean_by_university / teachers_mean_by_rf),
+    percent_of_region='{0:0.2%}'.format(teachers_mean_by_university / teachers_mean_by_region),
+    top_salary=round(teachers_top_by_university['salary'] / 1000, 0),
+    top_vacancy=teachers_top_by_university['title'],
+    top_url=teachers_top_by_university['url']
+)
 fig, ax = plt.subplots()
 ax.bar(stat_by_teachers['title'], stat_by_teachers['value'], color='green')
 ax.set_ylim(0, stat_by_teachers['value'].max() * 1.15)
@@ -42,6 +56,20 @@ stat_by_researcher = pd.DataFrame([
     {'title': region, 'value': df_researcher[df_researcher['region'] == region]['salary'].mean() / 1000},
     {'title': university, 'value': df_researcher[df_researcher['university_abbreviation'] == university]['salary'].mean() / 1000},
 ]).dropna()
+researchers_mean_by_rf = df_researcher['salary'].mean() / 1000
+researchers_mean_by_region = df_researcher[df_researcher['region'] == region]['salary'].mean() / 1000
+researchers_mean_by_university = df_researcher[df_researcher['university_abbreviation'] == university]['salary'].mean() / 1000
+researchers_top_by_university = df_researcher[df_researcher['university_abbreviation'] == university].sort_values(by='salary', ascending=False).iloc[0]
+data.loc['description_researcher', 'value'] = texts['empty'].format(
+    prof_role='Учитель, преподаватель, педагог'
+) if university not in list(stat_by_researcher['title'].unique()) else texts['not_empty'].format(
+    prof_role='Научный специалист, исследователь',
+    percent_of_rf='{0:0.2%}'.format(researchers_mean_by_university / researchers_mean_by_rf),
+    percent_of_region='{0:0.2%}'.format(researchers_mean_by_university / researchers_mean_by_region),
+    top_salary=round(researchers_top_by_university['salary'] / 1000, 0),
+    top_vacancy=researchers_top_by_university['title'],
+    top_url=researchers_top_by_university['url']
+)
 fig, ax = plt.subplots()
 ax.bar(stat_by_researcher['title'], stat_by_researcher['value'], color='orange')
 ax.set_ylim(0, stat_by_researcher['value'].max() * 1.15)
@@ -100,8 +128,8 @@ plt.close(fig)
 
 df_top_by_salary = df[~df['salary'].isnull()].sort_values(by='salary', ascending=False).reset_index()[:10]
 annex_top_by_salary = df_top_by_salary[['title', 'salary', 'university_abbreviation', 'url']]
-description_top_by_salary = texts['top_by_salary'].format(
-    '; '.join([f"{i} ({x['id']})" for i, x in df_top_by_salary[['region', 'id']].groupby(by='region').count().sort_values(by='id', ascending=False).iterrows()]),
-    len(df_top_by_salary['professional_roles'].unique()),
-    '; '.join([f"{i} ({x['id']})" for i, x in df_top_by_salary[['professional_roles', 'id']].groupby(by='professional_roles').count().sort_values(by='id', ascending=False).iterrows()])
+data.loc['top_vacancies', 'value'] = texts['top_by_salary'].format(
+    regions='; '.join([f"{i} ({x['id']})" for i, x in df_top_by_salary[['region', 'id']].groupby(by='region').count().sort_values(by='id', ascending=False).iterrows()]),
+    count=len(df_top_by_salary['professional_roles'].unique()),
+    prof_roles='; '.join([f"{i} ({x['id']})" for i, x in df_top_by_salary[['professional_roles', 'id']].groupby(by='professional_roles').count().sort_values(by='id', ascending=False).iterrows()])
 )
